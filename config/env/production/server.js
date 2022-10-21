55f41efc9b1415b2a4b7f48f535c483adc93bcbf
samnsimson <samnishanth01@gmail.com>
2022-10-21 08:59:12 +0530

    test

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,8 +1,10 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: "https://main.d3c0xiwj0ar5bz.amplifyapp.com/admin",
   app: {
     keys: env.array("APP_KEYS"),
+  },
+  admin: {
+    url: "/",
   },
 });
